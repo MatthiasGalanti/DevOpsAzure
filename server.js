2d9f0111b641baff050be71fa5bc0d7d1c199943
Matthias Galanti <matthias.galanti@halter.ch>
2024-05-14 17:46:27 +0200

    change_2_14.04.2024

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('DevOps is fun! Galanmat - FS2024');
+  res.send('DevOps is fun! Galanmat - FS2024 - Versuch 2');
 });
 
 app.listen(PORT, HOST);
